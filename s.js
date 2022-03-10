cce1ce546c5b103d87c8ca9a866367a2e5c03e36
Jorge Antonio <jorgeantonio.acll@gmail.com>
2022-03-10 19:47:21 -0300

    Final

--- s.js
+++ s.js
@@ -1,1 +1,19 @@
-alert("oi")
+
+function carregar() {
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours() 
+    msg.innerHTML = `agora são ${hora} horas`
+    
+    if (hora >= 0 && hora < 12) {
+        document.body.style.background = "#acddfd"
+        img.src = "img/manha.png"
+    } else if (hora >= 12 && hora < 18) {
+        document.body.style.background = "#ffba71"
+        img.src = "img/foto-tarde.png"
+    } else {
+        document.body.style.background = "#3a6166"
+        img.src = "img/noite.png"
+    }
+}
